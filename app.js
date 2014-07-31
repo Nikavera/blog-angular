2ada076a0c583deba00e4615b230b8a4cbdfc9f3
Veranika <veranika_baradzina@epam.com>
2014-07-31 11:05:41 +0300

    Init commit

---
+++ app.js
@@ -0,0 +1,43 @@
+(function() {
+
+	var app = angular.module('blog', []);
+	app.controller('BlogController', function($scope) {
+		this.articles = posts;
+		this.visiblePopup = false;
+		this.showPopup = function() {
+			this.visiblePopup = true;
+		};
+		this.clearForm = function() {
+			$scope.article = {};
+		};
+		this.isPopup = function() {
+			return this.visiblePopup;
+		};
+		this.addPost = function() {
+			var a = new Date();
+			$scope.article.date = a.getTime();
+			this.articles.push($scope.article);
+			this.clearForm();
+			this.closePopup();
+		};
+		this.closePopup = function() {
+			this.visiblePopup = false;
+		};
+	});
+
+	var posts = [
+		{
+			title: 'Blog title 1',
+			author: 'Author 1',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iusto placeat aliquid tempore harum, similique!',
+			date: '1400000100845',
+			image: 'images/blog-img.jpg'
+		},
+		{
+			title: 'Blog title 2',
+			author: 'Author 2',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, placeat est. Alias illo hic quo nobis, aspernatur iste ut voluptate.',
+			date: '1406000101845'
+		}
+	];
+})();
